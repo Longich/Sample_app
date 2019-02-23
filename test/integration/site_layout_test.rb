7abb4e78ed98f09d577329939e0eb57320e2242e
Longich <ka.kazu.kazuki@gmail.com>
2019-02-24 03:30:21 +0900

    Finish layout and routes

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,6 @@
-require "test_helper"
+require 'test_helper'
 
-class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -8,5 +8,9 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get contact_path
+    assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
